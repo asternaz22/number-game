4df28e3440429cce647c06022a302c37195ed533
asternaz22 <48803751+asternaz22@users.noreply.github.com>
2019-07-23 01:38:29 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+// let guess = "";
+
+
+
+
+// let guessNum = ["too small", "too big", "congratulations"],
+
+
+
+
+// function checkGuess() {
+
+//     let guess = document.querySelector("input").value;
+//     if (guess == false) {
+//         alert("please enter number");
+//     } else {
+//         if()
+//     }
+// }
+
+
+
+
+
+
+// function checkGuess() {
+
+//     let number = Math.floor(Math.random()*10);
+//     document.querySelector(number);
+//     let guess = prompt("Guess a number: ");
+//     do {
+//         guess = prompt("Keep guessing!");
+//         if (number < guess) {
+//             prompt("You've guessed too high!");
+//         } else if (number > guess) {
+//             prompt("You've guessed too low!");
+//         } else document.write("Good Job!");
+//     } while (guess != number);
+
+// }
+
+
+
+
+
+
+function checkGuess() {
+    let number= parseInt(Math.random() * 100) + 1;
+    numberOfGuesses = 0;
+    alert("enter number");
+    if 
+    return (guess > 0 && guess < 101);
+} 
+ 
