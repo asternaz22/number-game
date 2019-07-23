9373ad98e809ca3b5eaaee153dca9dc794db1c5e
asternaz22 <48803751+asternaz22@users.noreply.github.com>
2019-07-23 17:27:31 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,54 +1,30 @@
-// let guess = "";
+let randomNumber;
+let wrongGuessCount;
 
+function resetGame() {
+    randomNumber = parseInt(Math.random() * 100);
+    wrongGuessCount = 0;
+}
 
-
-
-// let guessNum = ["too small", "too big", "congratulations"],
-
-
-
-
-// function checkGuess() {
-
-//     let guess = document.querySelector("input").value;
-//     if (guess == false) {
-//         alert("please enter number");
-//     } else {
-//         if()
-//     }
-// }
-
-
-
-
-
-
-// function checkGuess() {
-
-//     let number = Math.floor(Math.random()*10);
-//     document.querySelector(number);
-//     let guess = prompt("Guess a number: ");
-//     do {
-//         guess = prompt("Keep guessing!");
-//         if (number < guess) {
-//             prompt("You've guessed too high!");
-//         } else if (number > guess) {
-//             prompt("You've guessed too low!");
-//         } else document.write("Good Job!");
-//     } while (guess != number);
-
-// }
-
-
-
-
-
+resetGame();
 
 function checkGuess() {
-    let number= parseInt(Math.random() * 100) + 1;
-    numberOfGuesses = 0;
-    alert("enter number");
-    if 
-    return (guess > 0 && guess < 101);
-} 
- 
+
+    let number = parseInt(document.querySelector("input").value);
+
+
+    if (number == randomNumber) {
+        alert("congratulations! you are smart ;) and your wrong guesses: " + wrongGuessCount);
+        resetGame();
+    } else {
+        if (number > randomNumber) {
+            alert(" too big! try again");
+            wrongGuessCount++;
+        } else {
+            alert(" too small!  try again");
+            wrongGuessCount++;
+        }
+    }
+}
+
+// REFACTOR
